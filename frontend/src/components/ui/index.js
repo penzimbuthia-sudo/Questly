2aeb45ab5b04514636d2d5dcf85d22561d31a660
penzimbuthia-sudo <penzifavour@gmail.com>
2026-08-19 00:05:45 +0300

    feat(ui): add barrel export for ui components

---
+++ frontend/src/components/ui/index.js
@@ -0,0 +1,5 @@
+export { default as Button } from "./Button";
+export { default as Card } from "./Card";
+export { default as Pill } from "./Pill";
+export { default as ProgressBar } from "./ProgressBar";
+export { default as Avatar } from "./Avatar";
