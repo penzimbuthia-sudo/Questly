2de2d490708a999c5dfc8eaa211505510b3c809c
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-08-19 13:38:22 +0300

    Include user role home links in AppRoutes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,3 +2,9 @@ import RoleRoute from './RoleRoute';
 import LearnerRoutes from './learnerRoutes';
 import ContributorRoutes from './contributorRoutes';
 import AdminRoutes from './adminRoutes';
+
+const ROLE_HOME = {
+  learner: '/learner',
+  contributor: '/contributor',
+  admin: '/admin',
+};
