d10c3fffe844a17467ebf521b17267df6995bb75
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-08-19 12:18:31 +0300

    Add the export default function in NotFound

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,1 +1,7 @@
 import { Link } from 'react-router-dom';
+
+export default function NotFound() {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center bg-[#10151C] px-4 text-center"></div>
+    )
+}
