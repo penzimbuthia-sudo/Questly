b6ae7f2248c58824a596e032841955e681cc8678
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-08-19 12:33:05 +0300

    Add the defined methods in api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -47,3 +47,11 @@ async function request(path, { method = 'GET', body, headers = {}, auth = true }
 
   return data;
 }
+
+export const api = {
+  get: (path, opts) => request(path, { ...opts, method: 'GET' }),
+  post: (path, body, opts) => request(path, { ...opts, method: 'POST', body }),
+  put: (path, body, opts) => request(path, { ...opts, method: 'PUT', body }),
+  patch: (path, body, opts) => request(path, { ...opts, method: 'PATCH', body }),
+  delete: (path, opts) => request(path, { ...opts, method: 'DELETE' }),
+};
