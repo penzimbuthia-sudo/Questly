5143b7dce5d59e539d714b78f3e3a93fec59f543
RecursiveLeo <leothuku893@gmail.com>
2026-08-21 12:17:44 +0300

    Update the imports in Landing.jsx

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,21 +1,12 @@
 // feature/your-name/landing-page/Landing.jsx
 <<<<<<< HEAD
-import Navbar from '../components/Landing/Navbar'
-import Hero from '../components/Landing/Hero'
-import FeatureGrid from '../components/Landing/FeaturedGrid'
-import HowItWorks from '../components/Landing/HowItWorks'
-import RolesShowcase from '../components/Landing/RolesShowcase'
-import CTASection from '../components/Landing/CTASection'
-import Footer from '../components/Landing/Footer'
-=======
 import Navbar from '../components/landing/Navbar'
 import Hero from '../components/landing/Hero'
-import FeatureGrid from '../components/landing/FeatureGrid'
+import FeatureGrid from '../components/landing/FeaturedGrid'
 import HowItWorks from '../components/landing/HowItWorks'
 import RolesShowcase from '../components/landing/RolesShowcase'
 import CTASection from '../components/landing/CTASection'
 import Footer from '../components/landing/Footer'
->>>>>>> 29beada173445223fcce41c2ed4682c85bcd3a9f
 
 export default function Landing() {
   return (
