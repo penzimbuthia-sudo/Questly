68545f1d5484b2412ef8b02e7c837884d56553b5
Chome <chomeallan@gmail.com>
2026-08-21 14:24:10 +0300

    Merge conflcict in Landing.jsx

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,13 +1,5 @@
 // feature/your-name/landing-page/Landing.jsx
-<<<<<<< HEAD
-import Navbar from '../components/Landing/Navbar'
-import Hero from '../components/Landing/Hero'
-import FeatureGrid from '../components/Landing/FeaturedGrid'
-import HowItWorks from '../components/Landing/HowItWorks'
-import RolesShowcase from '../components/Landing/RolesShowcase'
-import CTASection from '../components/Landing/CTASection'
-import Footer from '../components/Landing/Footer'
-=======
+
 import Navbar from '../components/landing/Navbar'
 import Hero from '../components/landing/Hero'
 import FeatureGrid from '../components/landing/FeatureGrid'
@@ -15,7 +7,7 @@ import HowItWorks from '../components/landing/HowItWorks'
 import RolesShowcase from '../components/landing/RolesShowcase'
 import CTASection from '../components/landing/CTASection'
 import Footer from '../components/landing/Footer'
->>>>>>> 29beada173445223fcce41c2ed4682c85bcd3a9f
+
 
 export default function Landing() {
   return (
