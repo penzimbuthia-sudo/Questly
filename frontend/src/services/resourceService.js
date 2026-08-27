516ddad86e730d52c6b9fbd22a3878645694a149
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-08-27 21:07:53 +0300

    Fix the api imports in these service files

--- frontend/src/services/resourceService.js
+++ frontend/src/services/resourceService.js
@@ -2,15 +2,15 @@ import { api } from "../services/api";
 
 export async function getMyResources() {
   const response = await api.get("/contributor/resources");
-  return response.data;
+  return response;
 }
 
 export async function createResource(data) {
   const response = await api.post("/contributor/resources", data);
-  return response.data;
+  return response;
 }
 
 export async function createLearningPath(data) {
   const response = await api.post("/contributor/learning-paths", data);
-  return response.data;
+  return response;
 }
