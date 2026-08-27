0a76a48fc2dc53b6dd548c54f669b533dd7a14d8
penzimbuthia-sudo <penzifavour@gmail.com>
2026-08-27 15:18:48 +0300

    add barrel files

---
+++ frontend/src/components/contributor/index.js
@@ -0,0 +1,6 @@
+export { default as StatCard } from "./StatCard";
+export { default as ContentCard } from "./ContentCard";
+export { default as ChallengeCard } from "./ChallengeCard";
+export { default as ContributorLeaderboard } from "./ContributorLeaderboard";
+export { default as AddResourceModal } from "./AddResourceModal";
+export { default as CreatePathModal } from "./CreatePathModal";
