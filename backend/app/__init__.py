b635b094a11ecfe612e749b1267960eb5e32fc10
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-08-28 09:49:48 +0300

    Define the function for initializing and setting up app

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,3 +3,12 @@ from flask import Flask, jsonify
 from app.config import config_by_name
 from app.extensions import cors, db, jwt, mail, migrate
 
+def create_app(config_name="development"):
+    app = Flask(__name__)
+    app.config.from_object(config_by_name[config_name])
+
+    db.init_app(app)
+    jwt.init_app(app)
+    migrate.init_app(app, db)
+    mail.init_app(app)
+    cors.init_app(app, resources={r"/*": {"origins": app.config["FRONTEND_URL"]}})
