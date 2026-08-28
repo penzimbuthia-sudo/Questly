d2081ebbbcaf00925e0b043a1a709d824da31e8f
roy9 <roymurimi527@gmail.com>
2026-08-28 09:52:41 +0300

    feat(backend): add schemas __init__ with all schema imports

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,7 @@
+from .user_schema import UserResponse, UserCreate, UserUpdate
+from .discussion_schema import DiscussionBase, DiscussionCreate, DiscussionUpdate, DiscussionResponse
+from .report_schema import ReportBase, ReportCreate, ReportUpdate, ReportResponse, ReportStatus
+from .resource_schema import ResourceBase, ResourceCreate, ResourceUpdate, ResourceResponse
+from .learning_path_schema import LearningPathBase, LearningPathCreate, LearningPathUpdate, LearningPathResponse
+from .challenge_schema import ChallengeBase, ChallengeCreate, ChallengeUpdate, ChallengeResponse
+from .quiz_schema import QuizBase, QuizCreate, QuizUpdate, QuizResponse
