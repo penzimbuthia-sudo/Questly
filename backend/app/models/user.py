47387a707d0981fc748f9070a9b19e00165b1336
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-08-28 09:19:50 +0300

    Include the uuid generation function and add the User class

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -4,3 +4,15 @@ from datetime import datetime
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from app.extensions import db
+
+
+def generate_uuid():
+    return str(uuid.uuid4())
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.String(36), primary_key=True, default=generate_uuid)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False, index=True)
+    password_hash = db.Column(db.String(255), nullable=False)
