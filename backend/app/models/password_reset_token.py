72290e0666c8930fe02d8ac84a6e9e66a3097814
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-08-28 09:15:19 +0300

    Add the functions that generate the uuid and token

--- backend/app/models/password_reset_token.py
+++ backend/app/models/password_reset_token.py
@@ -3,3 +3,10 @@ import uuid
 from datetime import datetime, timedelta
 
 from app.extensions import db
+
+def generate_uuid():
+    return str(uuid.uuid4())
+
+
+def generate_token():
+    return secrets.token_urlsafe(32)
