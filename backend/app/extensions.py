aae4fe76a0c69ed060a00621a1f67373174133ce
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-08-28 10:19:37 +0300

    Instantiate the extensions in extensions

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -3,3 +3,9 @@ from flask_jwt_extended import JWTManager
 from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+jwt = JWTManager()
+migrate = Migrate()
+mail = Mail()
+cors = CORS()
