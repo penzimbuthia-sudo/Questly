5a684168fb8dab4292ade59141ca562d0e5449a8
penzimbuthia-sudo <penzifavour@gmail.com>
2026-08-30 23:29:00 +0300

    feat(admin): add barrel export for admin components

---
+++ frontend/src/components/admin/index.js
@@ -0,0 +1,7 @@
+export { default as StatCard } from "./StatCard";
+export { default as ReviewQueueCard } from "./ReviewQueueCard";
+export { default as EditStatusModal } from "./EditStatusModal";
+export { default as ChallengeAdminCard } from "./ChallengeAdminCard";
+export { default as BadgeAdminCard } from "./BadgeAdminCard";
+export { default as ReportCard } from "./ReportCard";
+export { default as SystemHealthCard } from "./SystemHealthCard";
